81735fce10be5104b5f162eb3345eb25346da8e3
skarev <sevenkss95@gmail.com>
2020-08-24 14:05:07 +0300

    Initial Commit

---
+++ TneMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TneMovie
+//
+//  Created by skarev on 24.08.2020.
+//  Copyright © 2020 skarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
